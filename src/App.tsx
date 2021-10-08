4d5098240ae145f843026b5b31a80190b39cd736
renross <briross@udel.edu>
2021-10-08 00:01:11 -0400

    added logic for decrease button button works in console but does not display yet homepage link added to node.js.yml custom button file added git config updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "./components/CustomButtonComponent";
 
-function App(): JSX.Element {
+let counterDecrease = 0;
+
+function App(this: any): JSX.Element {
+    
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+                    Super cool and very well thought out welcome message!
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
+                <div style={{position:"fixed", right:300, top:450}}> {counterDecrease} </div>
+                <div style = {{ position:"fixed", right:300}}> 
+                    <Button 
+                        borderColor="none"
+                        color="none"
+                        height="20px" 
+                        width={"75px"}                
+                        onClick={ decreaseCounter }> Decrease </Button> 
+                </div>
+
             </header>
+            
         </div>
     );
 }
+
+const decreaseCounter = () => {
+    counterDecrease -= 1;
+    console.log(counterDecrease);
+};
 
 export default App;
