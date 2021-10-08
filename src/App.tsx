fb3085ffeeb511f9a02e51491de7db4e15190940
renross <briross@udel.edu>
2021-10-08 01:50:28 -0400

    added logic for increase button both counters now display the decrease and increase functions have been combined (udpateCounter)

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,60 @@
-import React, { Component } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React, { useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "./components/CustomButtonComponent";
 
-let counterDecrease = 0;
-
-function App(this: any): JSX.Element {
-    
+function App(this: any): JSX.Element {  
+    let buttonID = "";
+    const [counterIncrease, updateIncrease] = useState(0);
+    const [counterDecrease, updateDecrease] = useState(0);
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Super cool and very well thought out welcome message!
+                    Super cool and very well-thought-out welcome message!
                 </p>
-                <div style={{position:"fixed", right:300, top:450}}> {counterDecrease} </div>
-                <div style = {{ position:"fixed", right:300}}> 
+                <div style={{ position:"relative", right:660, bottom:250 }}> {counterIncrease} </div>
+                <div style = {{ position:"relative", right:700, bottom:230 }}> 
                     <Button 
                         borderColor="none"
-                        color="none"
+                        color="cyan"
                         height="20px" 
-                        width={"75px"}                
-                        onClick={ decreaseCounter }> Decrease </Button> 
+                        width={"100px"}                
+                        onClick={ () => {
+                            buttonID = "increase";
+                            updateCounter(buttonID, counterIncrease, updateIncrease);
+                        }}> Increase Me! </Button> 
+                </div>
+                <div style={{ position:"relative", left:740, bottom:320 }}> {counterDecrease} </div>
+                <div style = {{ position:"relative", left:700, bottom:300 }}> 
+                    <Button 
+                        borderColor="none"
+                        color="coral"
+                        height="20px" 
+                        width={"100px"}                
+                        onClick={ () => {
+                            buttonID = "decrease";
+                            updateCounter(buttonID, counterDecrease, updateDecrease);
+                        }}> Decrease Me! </Button> 
                 </div>
 
-            </header>
-            
-        </div>
+            </header></div>
     );
 }
 
-const decreaseCounter = () => {
-    counterDecrease -= 1;
-    console.log(counterDecrease);
-};
+function updateCounter(buttonID: string, counterUpdate: number, updateFunc: { (value: React.SetStateAction<number>): void; (arg0: number): void; }) {
+    if (buttonID == "increase") {
+        counterUpdate += 1;
+        updateFunc(counterUpdate);
+    } else if (buttonID == "decrease"){
+        counterUpdate -= 1;
+        updateFunc(counterUpdate);
+    } else {
+        console.log("well that's not good");
+    }
+}
 
 export default App;
